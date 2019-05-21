c57ad1f2d10610d753fa32034ea31757d571e3fe
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2019-05-21 17:53:27 +0000

    comma

--- src/process.py
+++ src/process.py
@@ -17,7 +17,7 @@ def main():
         # If there is no connection subprocess throws a 'CalledProcessError'
         pass
 
-    graphyte.init(host=telemetry_target_host, port=telemetry_target_port prefix=telemetry_prefix)
+    graphyte.init(host=telemetry_target_host, port=telemetry_target_port, prefix=telemetry_prefix)
     graphyte.send('foo.baz', 42)
 
 if __name__ == "__main__":
