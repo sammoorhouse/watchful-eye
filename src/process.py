79c10559bdf8e8ee8e0d4fa2ff150e57eb90e167
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2019-05-21 17:09:16 +0000

    graphyte

--- src/process.py
+++ src/process.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
 import subprocess
-
+import graphyte
+import os
 
 def main():
-    print("hello, world!")
+    telemetry_target_host = os.getenv('TELEMETRY_TARGET_HOST')
+    telemetry_target_port = os.getenv('TELEMETRY_TARGET_PORT', '2013')
+
+    telemetry_target = telemetry_target_host + ':' + telemetry_target_port
+
+    print(telemetry_target)
 
     SSID = None
     try:
-        SSID = subprocess.check_output(["iwgetid", "-r"]).strip()
+        SSID = str(subprocess.check_output(["iwgetid", "-r"]).strip(), "utf-8")
     except subprocess.CalledProcessError:
+        print("couldn't get SSID")
         # If there is no connection subprocess throws a 'CalledProcessError'
         pass
 
-    print("SSID: " + str(SSID, "utf-8"))
+    graphyte.init(telemetry_target, prefix='io.turntabl')
+    graphyte.send('foo.bar', 42, tags={'SSID': SSID})
 
 if __name__ == "__main__":
     main()
