d27218904239abf7fb7b3164a1d77670362f50c4
Sam Moorhouse <sam.j.moorhouse@gmail.com>
2019-05-21 16:58:05 +0000

    print

--- src/process.py
+++ src/process.py
@@ -1,1 +1,19 @@
-print("hello world")
+#!/usr/bin/env python
+
+import subprocess
+
+
+def main():
+    print("hello, world!")
+
+    SSID = None
+    try:
+        SSID = subprocess.check_output(["iwgetid", "-r"]).strip()
+    except subprocess.CalledProcessError:
+        # If there is no connection subprocess throws a 'CalledProcessError'
+        pass
+
+    print("SSID: " + SSID)
+
+if __name__ == "__main__":
+    main()
